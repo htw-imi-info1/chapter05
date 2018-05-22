64257def57b4be36853051d4702fd2f485f22fc6
B Kleinen <kleinen@infrastructure.de>
2018-05-22 07:01:31 +0200

    done in class

--- Movies/Movie.java
+++ Movies/Movie.java
@@ -1,13 +1,20 @@
 
 public class Movie
 {
+    private String title;
     private int releaseYear = 0;
     private boolean watched;
+    public Movie(String title, boolean watched, int releaseYear){
+        this.title = title;
+        this.watched = watched;
+        this.releaseYear = releaseYear;
+    }
+
     public int getReleaseYear(){return releaseYear;}
 
     public boolean getWatched(){return watched;}
 
     public String toString(){
-        return "Movie - year: "+releaseYear+ (watched ? "" : "not ") + "watched"; 
+        return "Movie "+title+" - year: "+releaseYear+ (watched ? " " : " not ") + "watched"; 
     }
 }
--- Movies/MovieDatabase.java
+++ Movies/MovieDatabase.java
@@ -3,6 +3,20 @@ import java.util.ArrayList;
 public class MovieDatabase
 {
     ArrayList<Movie> movies = new ArrayList<>();
+
+    public MovieDatabase(){
+        generateSomeTestData();
+    }
+
+    private void generateSomeTestData(){
+        movies.add(new Movie("a",true,1950));
+        movies.add(new Movie("b",false,1950));
+        movies.add(new Movie("c",true,1980));
+        movies.add(new Movie("d",true,1950));
+        movies.add(new Movie("e",true,1951));
+
+    }
+
     public void printWatchedInYear(int year){
         movies.stream()
         .filter(movie -> movie.getReleaseYear() == year)
