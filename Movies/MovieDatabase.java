1f07fd5b85cc242cc8db5fb9a80f91a410a3d793
B Kleinen <kleinen@infrastructure.de>
2018-05-22 07:46:12 +0200

    added example to construct newline separated string with streams

--- Movies/MovieDatabase.java
+++ Movies/MovieDatabase.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 public class MovieDatabase
 {
     ArrayList<Movie> movies = new ArrayList<>();
@@ -14,7 +15,7 @@ public class MovieDatabase
         movies.add(new Movie("c",true,1980));
         movies.add(new Movie("d",true,1950));
         movies.add(new Movie("e",true,1951));
-
+        movies.add(new Movie("f",false,1950));
     }
 
     public void printWatchedInYear(int year){
@@ -22,6 +23,18 @@ public class MovieDatabase
         .filter(movie -> movie.getReleaseYear() == year)
         .filter(movie -> movie.isWatched())
         .forEach(movie -> System.out.println(movie.toString()));
+    }
+
+    public String getWatchedInYear(int year){
+        return movies.stream()
+        .filter(movie -> movie.getReleaseYear() == year)
+        .filter(movie -> movie.isWatched())
+        .map(movie -> movie.toString())
+        .collect(Collectors.joining("\n"));
+    }
+
+    public void demo1(){
+        System.out.println(getWatchedInYear(1950));
     }
 
 }
